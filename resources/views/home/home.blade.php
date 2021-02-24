e8228c12fd8826c5b5386686c94944490367f5b9
MKA <mka@khestep.edu>
2021-02-24 21:27:00 +0200

    mvc

---
+++ resources/views/home/home.blade.php
@@ -0,0 +1,5 @@
+@extends('default')
+
+@section('content')
+    East or West but home is best!
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
+use App\Http\Controllers\PostController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('home', [HomeController::class, 'home'])->name('home');
+
+Route::get('/cabinet', function() {
+    return 'logic page';
+})->name('login');
+
+Route::get('/posts', [PostController::class, 'index'])->name('posts');
